e0c7add2b869422d6961b4b87e6dfdb4a39ad525
BayajidAlam <bayajidalam2001@gmail.com>
2023-02-25 14:00:00 +0600

    fix leack

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express");
+require('dotenv').config();
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 // mongo uri 
-const uri = `mongodb+srv://karata_user:srfKi98AFIqtjiQC@myclaster-1.wxhqp81.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@myclaster-1.wxhqp81.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { 
   useNewUrlParser: true, 
