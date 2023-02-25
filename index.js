70591f2f58596a7ca87d5d8bbf891730ac276dc5
BayajidAlam <bayajidalam2001@gmail.com>
2023-02-25 13:36:32 +0600

    service limit added for home

--- index.js
+++ index.js
@@ -8,10 +8,40 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 // mongo uri 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@myclaster-1.wxhqp81.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://karata_user:srfKi98AFIqtjiQC@myclaster-1.wxhqp81.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true, 
+  serverApi: ServerApiVersion.v1 
+});
 
+async function run(){
+  try{
+    const serviceCollection = client.db('karataDb').collection('services');
+
+    // get all services from db 
+    app.get('/services', async(req,res)=>{
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    // get limited services for home
+    app.get('/limitedServices', async(req,res)=>{
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const result = await cursor.limit(3).toArray();
+      res.send(result);
+    })
+  }
+  catch{
+
+  }
+
+
+}
+run().catch(err=>console.log(err));
 
 
 
