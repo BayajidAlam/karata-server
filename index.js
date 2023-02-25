79436a20af6ee6f660aedf423234305dd1ab6d81
BayajidAlam <bayajidalam2001@gmail.com>
2023-02-25 12:49:37 +0600

    server set up done

---
+++ index.js
@@ -0,0 +1,20 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+// mongo uri 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@myclaster-1.wxhqp81.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+app.listen(port, () => {
+  console.log(`App is listening on ${port}`);
+});
